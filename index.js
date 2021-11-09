9c8c44a2c2598057f0f876b5b6af4de40f653c49
ngaba1111 <ngaba1111@conestogac.on.ca>
2021-11-08 20:45:33 -0500

    port fix

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 //dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
+const port = process.env.PORT || 8000;
 var app = express();
-
-const port = process.env.PORT || 80;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
